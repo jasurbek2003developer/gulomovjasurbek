1f2c4bfc91b735874e2f8e35789a489cefae2749
Jasurbek G'ulomov <jasurbekgulomov@Jasurbeks-Air.mshome.net>
2022-08-30 10:49:31 +0500

    first web-site

---
+++ script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    loop:true,
+  });
+  window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
+  
+  
+  
+  
+  
